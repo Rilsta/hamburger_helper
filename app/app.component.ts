66b63d57b2a548f8c205bff514c628e495a5f58e
rilsta <rstarnes9@gmail.com>
2016-03-24 17:43:18 -0700

    add hard coded array of foods for testing functionality

--- app/app.component.ts
+++ app/app.component.ts
@@ -9,6 +9,16 @@ import { Food } from './food.model';
   </div>
   `
 })
-export class AppComponent{
-
+export class AppComponent {
+  public foods: Food[];
+  constructor(){
+    this.foods = [
+      new Food("Jose Burger", 1200, "Killer burger with peppahs"),
+      new Food("Beefy Five", 965, "Taco Bell Burrito"),
+      new Food("Basil Leaf", 5, "A single basil leaf")
+    ];
+  }
+  foodSelected(clickedFood: Food): void{
+    console.log(clickedFood)
+  }
 }
