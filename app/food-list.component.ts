de06c55246c58ffa118dcb66ed651bdfd101b633
rilsta <rstarnes9@gmail.com>
2016-03-25 09:25:58 -0700

    add Pipe to filter foods by calories

--- app/food-list.component.ts
+++ app/food-list.component.ts
@@ -1,20 +1,22 @@
 import { Component, EventEmitter } from 'angular2/core';
 import { FoodComponent } from './food.component'
+import { HealthPipe } from './health.pipe';
 import { Food } from './food.model';
 
 @Component({
   selector: 'food-list',
   inputs: ['foodList'],
   outputs: ['onFoodSelect'],
-  directives: [FoodComponent], [HealthPipe],
+  directives: [FoodComponent],
+  pipes: [HealthPipe],
   template: `
-  <select>
-    <option value="all">Show All Foods</option>
+  <select (change)="onChange($event.target.value)">
+    <option value="all" selected="selected">Show All Foods</option>
     <option value="healthy">Show Healthy Foods</option>
     <option value="unHealthy">Show Unhealthy Foods</option>
   </select>
 
-  <food-display *ngFor="#currentFood of foodList"
+  <food-display *ngFor="#currentFood of foodList | healthy:filterHealth"
     (click)="foodClicked(currentFood)"
     [class.selected]="currentFood === selectedFood"
     [food]="currentFood">
@@ -27,11 +29,15 @@ export class FoodListComponent {
   public foodList: Food[];
   public onFoodSelect: EventEmitter<Food>;
   public selectedFood: Food;
+  public filterHealth: string = "all"
   constructor() {
     this.onFoodSelect = new EventEmitter();
   }
   foodClicked(clickedFood: Food): void {
     this.selectedFood = clickedFood;
     this.onFoodSelect.emit(clickedFood);
+  }
+  onChange(filterOption) {
+    this.filterHealth = filterOption;
   }
 }
--- app/health.pipe.ts
+++ app/health.pipe.ts
@@ -2,22 +2,25 @@ import { Pipe, PipeTransform } from 'angular2/core';
 import { Food } from './food.model';
 
 @Pipe({
-  name: "healthy"
+  name: "healthy",
+  pure: false
 })
 
 export class HealthPipe implements PipeTransform {
   transform(input: Food[], args) {
     var desiredHealthState = args[0];
-    if(desiredHealthState === input.calories <= 300) {
+    if(desiredHealthState === "healthy") {
       return input.filter(function(food) {
-        return food.healthy;
+        return (food.calories <= 300);
       });
     } else if (desiredHealthState === "unHealthy") {
       return input.filter(function(food){
-        return !food.healthy;
+        return (food.calories > 300);
       });
-    } else {
-      return input;
+    } else if (desiredHealthState === "all") {
+      return input.filter(function(food){
+        return (food.calories > 0);
+      });
     }
   }
 }
